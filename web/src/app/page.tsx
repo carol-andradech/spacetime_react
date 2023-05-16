f21e8fa09ffcf56679a1061bbad66b403d68964a
Ana Carolina <carol.andradechagas@gmail.com>
2023-05-15 23:09:17 -0300

    changes text

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,8 +1,7 @@
 export default function Home() {
   return (
     <div className="h-screen bg-zinc-950 text-zinc-50 p-6">
-      <h1 className="text-4xl font-bold">Sua cápsula do tempo.</h1>
-      <p>test</p>
+      <h1 className="text-4xl font-bold">Sua cápsula do tempo!</h1>
     </div>
   );
 }
