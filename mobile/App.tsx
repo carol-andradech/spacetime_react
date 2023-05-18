19b2d53594b12b5e2bc0ef6e3b3bf7b156f095cd
Ana Carolina <carol.andradechagas@gmail.com>
2023-05-18 19:36:24 -0300

    creating model memories and http routes

---
+++ mobile/App.tsx
@@ -0,0 +1,67 @@
+import { StatusBar } from "expo-status-bar";
+import { ImageBackground, Text, View, TouchableOpacity } from "react-native";
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold,
+} from "@expo-google-fonts/roboto";
+
+import { BaiJamjuree_700Bold } from "@expo-google-fonts/bai-jamjuree";
+
+import blurBg from "./src/assets/bg-blur.png";
+import Stripes from "./src/assets/stripes.svg";
+import NLWLogo from "./src/assets/nlw-spacetime-logo.svg";
+import { styled } from "nativewind";
+
+//Permite usar tailwind no Stripes
+const StyledStripes = styled(Stripes);
+
+export default function App() {
+  const [hasLoadedFonts] = useFonts({
+    Roboto_400Regular,
+    Roboto_700Bold,
+    BaiJamjuree_700Bold,
+  });
+
+  if (!hasLoadedFonts) {
+    return null;
+  }
+
+  return (
+    <ImageBackground
+      source={blurBg}
+      className="bg-gray-900 px-8 py-10 relative flex-1 items-center"
+      imageStyle={{ position: "absolute", left: "-100%" }}
+    >
+      <StyledStripes className="absolute left-2" />
+      <View className="flex-1 items-center justify-center gap-6">
+        <NLWLogo />
+
+        <View className="space-y-2">
+          <Text className="text-center font-title text-2xl leading-tight text-gray-50">
+            Sua cápsula do tempo{" "}
+          </Text>
+          <Text className="text-center font-body text-base leading-relaxed text-gray-100">
+            Colecione momentos marcantes da sua jornada e compartilhe (se
+            quiser) com o mundo!{" "}
+          </Text>
+        </View>
+
+        <TouchableOpacity
+          activeOpacity={0.7}
+          className="rounded-full bg-green-500 px-5 py-2"
+        >
+          <Text className="font-alt text-sm uppercase text-black">
+            Cadastrar Lembrança
+          </Text>
+        </TouchableOpacity>
+      </View>
+
+      <Text className="text-center font-body text-sm leading-relaxed text-gray-200">
+        {" "}
+        Feito por Carol Andrade no NLW da Rocketseat 💜
+      </Text>
+      <StatusBar style="light" translucent />
+    </ImageBackground>
+  );
+}
