5b132192cb764d3b9afc9f493575cda2f47223ba
Ana Carolina <carol.andradechagas@gmail.com>
2023-06-05 16:24:53 -0300

    mobile - OAuth, token secure store, expo route

--- mobile/app/index.tsx
+++ mobile/app/index.tsx
@@ -8,20 +8,71 @@ import {
 
 import { BaiJamjuree_700Bold } from "@expo-google-fonts/bai-jamjuree";
 
-import blurBg from "./src/assets/bg-blur.png";
-import Stripes from "./src/assets/stripes.svg";
-import NLWLogo from "./src/assets/nlw-spacetime-logo.svg";
+import blurBg from "../src/assets/bg-blur.png";
+import Stripes from "../src/assets/stripes.svg";
+import NLWLogo from "../src/assets/nlw-spacetime-logo.svg";
 import { styled } from "nativewind";
+import { makeRedirectUri, useAuthRequest } from "expo-auth-session";
+import { useEffect } from "react";
+import { api } from "../src/lib/api";
+import * as SecureStore from "expo-secure-store";
+import { useRouter } from "expo-router";
 
 //Permite usar tailwind no Stripes
 const StyledStripes = styled(Stripes);
 
+const discovery = {
+  authorizationEndpoint: "https://github.com/login/oauth/authorize",
+  tokenEndpoint: "https://github.com/login/oauth/access_token",
+  revocationEndpoint:
+    "https://github.com/settings/connections/applications/673297a5a40c82d14387",
+};
+
 export default function App() {
+  const router = useRouter();
+
   const [hasLoadedFonts] = useFonts({
     Roboto_400Regular,
     Roboto_700Bold,
     BaiJamjuree_700Bold,
   });
+
+  const [request, response, signInWithGitHub] = useAuthRequest(
+    {
+      clientId: "673297a5a40c82d14387",
+      scopes: ["identity"],
+      redirectUri: makeRedirectUri({
+        scheme: "nlwspacetime",
+      }),
+    },
+    discovery
+  );
+
+  async function handleGitHuhOAuthCode(code: string) {
+    const response = await api.post("/register", {
+      code,
+    });
+
+    const { token } = response.data;
+    await SecureStore.setItemAsync("token", token);
+    router.push("/memories");
+    console.log(token);
+  }
+
+  //Toda a vez que a variável response mudar seu valor, executa o código
+  //O código verifica o tipo de response é sucess então obtenho o código do github
+  useEffect(() => {
+    //console.log(
+    //makeRedirectUri({
+    //scheme: "nlwspacetime",
+    // })
+    //  );
+
+    if (response?.type === "success") {
+      const { code } = response.params;
+      handleGitHuhOAuthCode(code);
+    }
+  }, [response]);
 
   if (!hasLoadedFonts) {
     return null;
@@ -50,6 +101,7 @@ export default function App() {
         <TouchableOpacity
           activeOpacity={0.7}
           className="rounded-full bg-green-500 px-5 py-2"
+          onPress={() => signInWithGitHub()}
         >
           <Text className="font-alt text-sm uppercase text-black">
             Cadastrar Lembrança
